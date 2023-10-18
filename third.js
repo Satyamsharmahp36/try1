44fc2b7b6a156f91d08550c0d9fdab081d135971
Satyam <satyamworks20@gmail.com>
2023-10-18 10:17:32 +0530

    commit me

---
+++ third.js
@@ -0,0 +1,84 @@
+var div = document.getElementById('q');
+var age = "<h2>this</h2>";
+div.textContent=age
+
+
+//declearing a variable
+ 
+var age ="s";
+console.log(age);
+
+var ab
+
+ab = ab+2
+console.log(ab)
+//const
+const xy = {a:2}
+xy.a="satyam"
+console.log(xy)
+//let
+ let b = 65;
+ 
+ b= b+2
+ console.log(b)
+
+ var test = null
+ console.log(test)
+ console.log(typeof(null))
+
+var st ="duwihwv ab"
+var tb = "kjjhbcev"
+console.log(st.concat(tb))
+console.log(st.replace(/ab/g,"xyz"))
+
+//array
+
+var a = ["a", 2, 3 ,"d"]
+console.log(a[2])
+a.push="ad"
+console.log(a)
+console.log(a.splice(1,0,"here"))
+console.log(a)
+
+//function
+
+function add(a,y){return a+y; }
+console.log(add(2,3))
+
+var multiply=(x,y)=>x*y
+console.log(multiply(1,3))
+//if else
+var a = "tru"
+if(a=="true"){console.log("yes its true")}
+else{console.log("its not that")}
+switch(true){
+    case a=="try":console.log("yes is try")
+    break;
+    case a=="true":"yes is true"
+    break;
+    default:console.log("no ists what ")
+    break;
+}
+function abcd (car,bike,bus){
+    this.car=car;
+    this.bike=bike;
+    this.bus=bus;
+}
+var a2= new abcd ("maruti","splinder","madza");
+console.log(abcd)
+//append
+var dadat = document.body;
+dadat.append("this is what i have appended","<h2>this</h2>")
+function printnumbers(a,b) {for (let f=a;f<=b;f++ ) {
+    console.log(f);}
+}
+console.log(printnumbers(1,20))
+let u =1
+do{
+    console.log(u);
+    u++;
+}while(u<30);
+
+
+   
+
